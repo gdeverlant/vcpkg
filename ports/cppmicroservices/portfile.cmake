b1eb9c564d94a33fe5c615c4900b9023f0274ced
ergü <ergu@erguder.tk>
2019-04-02 08:19:11 +0200

    Final revision

--- ports/cppmicroservices/portfile.cmake
+++ ports/cppmicroservices/portfile.cmake
@@ -16,14 +16,14 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO gdeverlant/CppMicroServices
-    REF 9e879d80b4752f1a290a54b89ab957a9beef3f4c
-    SHA512 62273765abf54330f93a90f1eccb26c6d3ed8c352b1c9dfda99e8ed266cd47348b03b72e011cdbcf2db4f011bb1faebcb369a754c64f6e545d143400768aa853
-    HEAD_REF port/vcpkg/cppmicroservices
+    REF b4dd3f8f35f9283f224c7112c3f58e7583c875e3
+    SHA512 f9f21df6dd8f5659d50484b2163dcd018febf08e2224826b8f06494f98a11fb80e6e3c8b380b99ee754315505cc390ccfc6668a949462efe62a6ed2b2afb010b
+    HEAD_REF port/5801/cppmicroservices
 )
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    #PREFER_NINJA # Disable this option if project cannot be built with Ninja
     # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
     # OPTIONS_RELEASE -DOPTIMIZE=1
     # OPTIONS_DEBUG -DDEBUGGABLE=1
@@ -38,14 +38,37 @@ message(STATUS "OUT_SOURCE_PATH = ${OUT_SOURCE_PATH}")
 message(STATUS "SOURCE_PATH = ${SOURCE_PATH}")
 message(STATUS "CURRENT_BUILDTREES_DIR = ${CURRENT_BUILDTREES_DIR}")
 
- # Tools require dlls
- vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools)
+# Tools require dlls
+#vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools)
 
 # Extract the name of the folder
 get_filename_component(FOLDER_NAME "${SOURCE_PATH}" NAME)
 # Handle copyright
 file(COPY ${CURRENT_BUILDTREES_DIR}/src/${FOLDER_NAME}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/cppmicroservices)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/cppmicroservices/LICENSE ${CURRENT_PACKAGES_DIR}/share/cppmicroservices/copyright)
+
+
+file(GLOB EXES_RELEASE
+    ${CURRENT_PACKAGES_DIR}/tools/cppmicroservices4/usResourceCompiler4.exe
+    ${CURRENT_PACKAGES_DIR}/tools/cppmicroservices4/usShell4.exe
+)
+file(GLOB EXES_DEBUG
+    ${CURRENT_PACKAGES_DIR}/debug/tools/cppmicroservices4/usResourceCompiler4.exe
+    ${CURRENT_PACKAGES_DIR}/debug/tools/cppmicroservices4/usShell4.exe
+)
+
+# file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/cppmicroservices4)
+# file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/cppmicroservices4/debug)
+
+ # Drop a copy of tools
+ file(COPY ${EXES_RELEASE} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/cppmicroservices4)
+ file(COPY ${EXES_DEBUG} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/cppmicroservices4/debug)
+
+ # Tools require dlls
+#  vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/cppmicroservices4)
+#  vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/cppmicroservices4/debug)
+
+ #file(REMOVE ${EXES_RELEASE} ${EXES_DEBUG})
 
 # Post-build test for cmake libraries
 # vcpkg_test_cmake(PACKAGE_NAME cppmicroservices)
